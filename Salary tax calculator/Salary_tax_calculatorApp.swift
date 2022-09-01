3bc8b22111e1c15f1c32f4dc64cffdd23095ada8
hivzi <hivzi@kkelmendi-mbp.none>
2022-09-01 22:50:33 +0200

    Initial Commit

---
+++ Salary tax calculator/Salary_tax_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salary_tax_calculatorApp.swift
+//  Salary tax calculator
+//
+//  Created by Hivzi on 1.9.22.
+//
+
+import SwiftUI
+
+@main
+struct Salary_tax_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
